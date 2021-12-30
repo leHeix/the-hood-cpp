9ab2d148d672a625ef340b5f3da85c2a30d794b5
leHeix <gordosatiro@protonmail.com>
2021-12-29 22:27:37 -0400

    Add CConsole patterns on Linux

--- src/hooks/CConsole.cpp
+++ src/hooks/CConsole.cpp
@@ -23,6 +23,9 @@ CConsole::CConsole()
 		}\
 		_##function##_fun = temp_addr
 
+	// Patterns taken from YSF
+	// https://github.com/IllidanS4/YSF/
+
 #ifdef _WIN32
 	SCAN_PATTERN(AddStringVariable, "\x53\x56\x57\x8B\x7C\x24\x18\x85\xFF", "xxxxxxxxx");
 	SCAN_PATTERN(GetStringVariable, "\x8B\x44\x24\x04\x50\xE8\x00\x00\x00\x00\x85\xC0\x74\x0B", "xxxxxx????xxxx");
@@ -35,6 +38,16 @@ CConsole::CConsole()
 	SCAN_PATTERN(SendRules, "\x81\xEC\x08\x04\x00\x00\x53\x55\x56\x57\x8B\xF9\x8B\x77\x04", "xx????xxxxxxxxx");
 	SCAN_PATTERN(Execute, "\x55\x8B\xEC\x83\xE4\xF8\x81\xEC\x0C\x01\x00\x00", "xxxxxxxxxxxx");
 #else
+	SCAN_PATTERN(AddStringVariable, "\x55\x89\xE5\x56\x53\x83\xEC\x00\x8B\x75\x00\x85\xF6\x74\x00\x89\x34\x24", "xxxxxxx?xx?xxx?xxx");
+	_GetStringVariable_fun = _AddStringVariable_fun - 0x760;
+	SCAN_PATTERN(SetStringVariable, "\x55\x89\xE5\x83\xEC\x00\x89\x75\x00\x8B\x45\x00\x89\x7D\x00\x8B\x7D\x00\x89\x5D\x00\x89\x44\x24\x00\x8B\x45\x00", "xxxxx?xx?xx?xx?xx?xx?xxx?xx?");
+	SCAN_PATTERN(SetIntVariable, "\x83\x38\x00\x74\x00\xC9\xC3\x8B\x50\x00\x8B\x45\x00", "xx?x?xxxx?xx?") - 0x1C;
+	_GetIntVariable_fun = _SetIntVariable_fun + 0x30;
+	_GetBoolVariable_fun = _SetIntVariable_fun - 0x30;
+	SCAN_PATTERN(ModifyVariableFlags, "\x89\x04\x24\xE8\x00\x00\x00\x00\x85\xC0\x89\xC2\x74\x00\x8B\x45\x00", "xxxx????xxxxx?xx?") - 0x10;
+	SCAN_PATTERN(FindVariable, "\xB9\xFF\x00\x00\x00\x89\xE5\x81\xEC\x68\x01\x00\x00", "xxxxxxxxxxxxx") - 0x1;
+	SCAN_PATTERN(SendRules, "\x55\x31\xD2\x89\xE5\x57\x56\x53\x81\xEC\x4C\x04", "xxxxxxxxxxxx");
+	SCAN_PATTERN(Execute, "\x55\x89\xE5\x57\x56\x53\x81\xEC\x3C\x01\x00\x00\x8B\x45\x0C", "xxxxxxxxxxxxxxx");
 #endif
 
 	#undef SCAN_PATTERN
